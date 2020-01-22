d07b1f55293f2f21b9ef01b86fbb8c2f37c86b85
Rodrigo Borges <rodrigoborges93@gmail.com>
2020-01-22 13:27:06 +0000

    Added email and password at prompt

--- connect.py
+++ connect.py
@@ -1,23 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-#import pyautogui as pag
+from getpass import getpass
+from getpass import getuser
 import time
 
-driver = webdriver.Firefox('') #PATH of webdriver
 
 def conectar(paginaInicial, paginaFinal):
     conectar = 0
     for i in range(paginaInicial, paginaFinal +1):
         time.sleep(1)
-        driver.get("https://www.linkedin.com/search/results/people/?facetIndustry=%5B%22102%22%5D&facetNetwork=%5B%22S%22%5D&origin=FACETED_SEARCH&page="+str(i)) # here you should place the url of a 'specific work area.
+        driver.get("https://www.linkedin.com/search/results/people/?facetGeoRegion=[%22au%3A0%22%2C%22br%3A6034%22%2C%22br%3A6368%22%2C%22us%3A0%22]&facetIndustry=[%22102%22]&facetNetwork=[%22S%22]&origin=FACETED_SEARCH&page="+str(i))
         time.sleep(1)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(1)
         conectButtons = driver.find_elements_by_xpath("//button[@class='search-result__action-button search-result__actions--primary artdeco-button artdeco-button--default artdeco-button--2 artdeco-button--secondary']")
         for button in conectButtons:
             time.sleep(1)
-            if button.text == 'Connect':
+            if button.text == 'Conectar':
+                print(button.text)
                 button.click()
                 time.sleep(1)       
                 driver.find_element_by_xpath("//button[@class='ml1 artdeco-button artdeco-button--3 artdeco-button--primary ember-view']").click()
@@ -29,11 +30,11 @@ def conectar(paginaInicial, paginaFinal):
     print(f'{conectar} invites were sent')
 
 
-def login(username, password):
+def login():
     username = driver.find_element_by_id('username')
-    username.send_keys(username)
+    username.send_keys(username_text)
     password = driver.find_element_by_id('password')
-    password.send_keys(password)
+    password.send_keys(password_text)
     time.sleep(1)
     login = driver.find_element_by_xpath("//button[@class='btn__primary--large from__button--floating']")
     login.click()
@@ -41,13 +42,17 @@ def login(username, password):
     
 
 def main():
-    url = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
+    global username_text, password_text, driver
     print('Started Linkedin Script')
+    username_text = input('E-mail: ')
+    password_text = getpass()
+    driver = webdriver.Firefox('C:\\Users\\b8m7\\Desktop\\linkedinBot')
+    url = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
     driver.get(url)
     time.sleep(3)
-    login('username', 'password')
+    login()
     time.sleep(2)
-    conectar(2,7)
+    conectar(1,7)
     print('Script Linkedin done!')
 
 
